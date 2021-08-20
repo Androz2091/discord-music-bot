b0f336caa26d58fcfd43750bb4a3ecbe8de5af7c
Androz2091 <androz2091@gmail.com>
2021-08-20 18:21:47 +0200

    :sparkles: Add np command

---
+++ commands/np.js
@@ -0,0 +1,38 @@
+const { SlashCommand } = require('slash-create');
+
+module.exports = class extends SlashCommand {
+    constructor(creator) {
+        super(creator, {
+            name: "np",
+            description: "See what's currently being played"
+        });
+    }
+
+    async run (ctx) {
+
+        const { client } = require('..');
+
+        await ctx.defer();
+
+        const queue = client.player.getQueue(ctx.guildID);
+        if (!queue || !queue.playing) return void ctx.sendFollowUp({ content: "❌ | No music is being played!" });
+        const progress = queue.createProgressBar();
+        const perc = queue.getPlayerTimestamp();
+
+        return void ctx.sendFollowUp({
+            embeds: [
+                {
+                    title: "Now Playing",
+                    description: `🎶 | **${queue.current.title}**! (\`${perc.progress}%\`)`,
+                    fields: [
+                        {
+                            name: "\u200b",
+                            value: progress
+                        }
+                    ],
+                    color: 0xffffff
+                }
+            ]
+        });
+    }
+}
