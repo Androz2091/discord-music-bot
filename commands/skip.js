5f5c3d1b72caec61ebc87b49d698cdaa49830d2a
Androz2091 <androz2091@gmail.com>
2021-08-20 18:15:38 +0200

    :sparkles: Add ski command

---
+++ commands/skip.js
@@ -0,0 +1,25 @@
+const { SlashCommand } = require('slash-create');
+
+module.exports = class extends SlashCommand {
+    constructor(creator) {
+        super(creator, {
+            name: "skip",
+            description: "Skip to the current song"
+        });
+    }
+
+    async run(ctx) {
+        
+        const { client } = require('..');
+        
+        await ctx.defer();
+        const queue = client.player.getQueue(ctx.guildID);
+        if (!queue || !queue.playing) return void ctx.sendFollowUp({ content: "❌ | No music is being played!" });
+        const currentTrack = queue.current;
+        const success = queue.skip();
+        return void ctx.sendFollowUp({
+            content: success ? `✅ | Skipped **${currentTrack}**!` : "❌ | Something went wrong!"
+        });
+
+    }
+}
