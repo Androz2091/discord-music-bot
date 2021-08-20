26725a95b9088c744ff6d24b0649653496954aec
Androz2091 <androz2091@gmail.com>
2021-08-20 18:15:47 +0200

    :sparkles: Add queue command

---
+++ commands/queue.js
@@ -0,0 +1,39 @@
+const { SlashCommand } = require('slash-create');
+
+module.exports = class extends SlashCommand {
+    constructor(creator) {
+        super(creator, {
+            name: "queue",
+            description: "See the queue"
+        });
+    }
+
+    async run(ctx) {
+        
+        const { client } = require('..');
+        
+        await ctx.defer();
+        const queue = client.player.getQueue(ctx.guildID);
+        if (!queue || !queue.playing) return void ctx.sendFollowUp({ content: "❌ | No music is being played!" });
+        const currentTrack = queue.current;
+        const tracks = queue.tracks.slice(0, 10).map((m, i) => {
+            return `${i + 1}. **${m.title}** ([link](${m.url}))`;
+        });
+
+        return void ctx.sendFollowUp({
+            embeds: [
+                {
+                    title: "Server Queue",
+                    description: `${tracks.join("\n")}${
+                        queue.tracks.length > tracks.length
+                            ? `\n...${queue.tracks.length - tracks.length === 1 ? `${queue.tracks.length - tracks.length} more track` : `${queue.tracks.length - tracks.length} more tracks`}`
+                            : ""
+                    }`,
+                    color: 0xff0000,
+                    fields: [{ name: "Now Playing", value: `🎶 | **${currentTrack.title}** ([link](${currentTrack.url}))` }]
+                }
+            ]
+        });
+
+    }
+}
